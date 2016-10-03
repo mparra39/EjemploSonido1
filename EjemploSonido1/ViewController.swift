6c0123c94f255615d48d9dc6d472c0bbd448df97
Mario Parra <mparrah39@gmail.com>
2016-10-03 13:39:33 -0500

    Initial Commit

---
+++ EjemploSonido1/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  EjemploSonido1
+//
+//  Created by Administrador on 30/09/16.
+//  Copyright © 2016 ITESO. All rights reserved.
+//
+
+import UIKit
+import AudioToolbox
+
+class ViewController: UIViewController {
+
+    private var miSonido: SystemSoundID = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let sonidoURL = Bundle.main.url(forResource: "prueba", withExtension: "aiff")
+    
+        AudioServicesCreateSystemSoundID(sonidoURL! as CFURL, &miSonido)
+    }
+    
+    
+    @IBAction func play() {
+        
+        AudioServicesPlaySystemSound(miSonido)
+    }
+
+
+}
+
